5063989e6d42e8a313fbf914dfaaca02d721c7c6
Петр goggins <petr-sadovnikov@mail.ru>
2026-08-10 10:27:11 +0300

    дз1-2

---
+++ friendly-spork-library-react-main/library-app/src/App.tsx
@@ -0,0 +1,9 @@
+import BooksPage from './pages/BooksPage/BooksPage';
+import ReadersPage from './pages/ReadersPage/ReadersPage';
+import ReaderProfilePage from './pages/ReaderProfilePage/ReaderProfilePage';
+function App() {
+return (
+<BooksPage /> // Меняйте страницу здесь вручную для проверки
+);
+}
+export default App;
